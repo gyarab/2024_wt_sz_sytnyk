5d756764834873dec10234027560aff45620bf73
nimayo <veronika.sytnyk.s@gyarab.cz>
2025-02-12 17:16:20 +0100

    zelva

---
+++ zelva.py
@@ -0,0 +1,26 @@
+from turtle import shape
+from turtle import forward, left, right, exitonclick
+from turtle import penup, pendown
+
+shape('turtle')
+
+penup()
+left(90)
+pendown()
+forward(50)
+right(90)
+forward(50)
+right(135)
+forward(70)
+right(225)
+forward(45)
+left(90)
+forward(50)
+left(30)
+forward(45)
+left(120)
+forward(45)
+left(75)
+forward(65)
+
+exitonclick()
